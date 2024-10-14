e43615dda7fd4430c866e5ed985d44083459f1e9
Roman750 <romancoletti@hotmail.com>
2024-10-14 17:18:56 -0300

    bustrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -59,6 +59,43 @@
         .suggestions-list li:hover {
             background: #f0f0f0;
         }
+
+    //estrella de favoritos
+        .star-checkbox {
+            position: relative;
+            cursor: pointer;
+            display: inline-block;
+        }
+
+        .star-checkbox input {
+            display: none; /* Oculta el checkbox original */
+        }
+
+        .star {
+            display: inline-block;
+            width: 20px;
+            height: 20px;
+            background-color: #ddd; /* Color de la estrella por defecto */
+            clip-path: polygon(
+                50% 0%,
+                61% 35%,
+                98% 35%,
+                68% 57%,
+                79% 91%,
+                50% 70%,
+                21% 91%,
+                32% 57%,
+                2% 35%,
+                39% 35%
+            );
+            
+            transition: background-color 0.3s;
+        }
+
+        .star-checkbox input:checked + .star {
+            background-color: #ffd700; /* Color de la estrella cuando está seleccionada */
+        }
+
     </style>
 </head>
 <body>
@@ -142,10 +179,12 @@
                     Latitud: ${busStop.latitude}<br>
                     Longitud: ${busStop.longitude}<br>
                     ID: ${busStop.id}<br>
-                    <form method="post" action="{{route('bus-stop.store')}}" id="formCheck">
-                        @csrf
-                        <label>Me gusta <input type="checkbox" value="${busStop.id}" id="paradaSeleccionada" name="paradaId" onchange="guardarParada()"></label>
-                    </form>
+                    <div class="container text-center mt-5">
+                        <form method="post" action="{{route('bus-stop.store')}}" id="formCheck">
+                            @csrf
+                            <label class="star-checkbox">Me gusta <input type="checkbox" class="d-none" value="${busStop.id}" id="paradaSeleccionada" name="paradaId" onchange="guardarParada()"><span class="star"></span></label>
+                        </form>
+                    </div> 
                 `);
                 markers.addLayer(marker); // Añadir cada marcador al grupo de clusters
             });
@@ -417,8 +456,10 @@
     })
     
     .bindPopup(`<b>Esta es la linea: ${ruta.nombre}</b><br>
-                Pertenece a la empresa: ${ruta.empresa}
-                <x-busicon color="${colorEmpresa}"></x-busicon>`)
+                Pertenece a la empresa: ${ruta.empresa}<br>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${colorEmpresa}" class="bi bi-bus-front-fill" viewBox="0 0 16 16">
+                    <path d="M16 7a1 1 0 0 1-1 1v3.5c0 .818-.393 1.544-1 2v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5V14H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2a2.5 2.5 0 0 1-1-2V8a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1V2.64C1 1.452 1.845.408 3.064.268A44 44 0 0 1 8 0c2.1 0 3.792.136 4.936.268C14.155.408 15 1.452 15 2.64V4a1 1 0 0 1 1 1zM3.552 3.22A43 43 0 0 1 8 3c1.837 0 3.353.107 4.448.22a.5.5 0 0 0 .104-.994A44 44 0 0 0 8 2c-1.876 0-3.426.109-4.552.226a.5.5 0 1 0 .104.994M8 4c-1.876 0-3.426.109-4.552.226A.5.5 0 0 0 3 4.723v3.554a.5.5 0 0 0 .448.497C4.574 8.891 6.124 9 8 9s3.426-.109 4.552-.226A.5.5 0 0 0 13 8.277V4.723a.5.5 0 0 0-.448-.497A44 44 0 0 0 8 4m-3 7a1 1 0 1 0-2 0 1 1 0 0 0 2 0m8 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m-7 0a1 1 0 0 0 1 1h2a1 1 0 1 0 0-2H7a1 1 0 0 0-1 1"/>
+                </svg>`)
     routes.addLayer(polyline)
     .addTo(map);
     })
