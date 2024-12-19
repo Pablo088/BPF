d8a1e9e3b736c9dc699ac96d5011073414b3c5bc
Pablo003 <martinezzppablo8@gmail.com>
2024-12-19 16:59:19 -0300

    mucho

--- resources/views/admin/user_rol.blade.php
+++ resources/views/admin/user_rol.blade.php
@@ -1,4 +1,7 @@
 @extends('adminlte::page')
+@section('content_header')
+    @vite('resources/css/admin/user_rol.css')    
+@stop
 @section('content')
 
     <table>
@@ -19,5 +22,30 @@
             @endforeach
         </tbody>
     </table>
+   
+    <div id="infoModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Información Detallada</h2>
+        <h3>Cambiar rol del usuario</h3>
+        <form action="{{route('users.manage.post',$id)}}" method="post">
+            @csrf
+            @method('PUT')
+            <select name="cambiarRol" id="cambiarRol">
+                <option value="Admin">Admin</option>
+                <option value="User">User</option>
+                <option value="Colectivero">Colectivero</option>
+            </select>
+            <button type="submit">Cambiar</button>
+        </form>
+
+        @foreach ($userLine as $relacion)
+            <div>El colectivero {{$relacion->name}} esta asociado a la linea {{$relacion->line_name}}</div>
+        
+        @endforeach
     
+        <p id="modalInfo">Detalles aquí...</p>
+    </div>
+    
+@vite('resources/js/admin/user_rol.js')
 @stop
